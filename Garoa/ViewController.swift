4bce499487c405433278f42cc7022ba145bb15f5
Lucas <lucas@agtlucas.com>
2016-02-15 21:15:53 -0200

    Updates

--- Garoa/ViewController.swift
+++ Garoa/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Mapbox
 
 class ViewController: UIViewController {
+    
+    var mapView: MGLMapView!
+    
+    let lat: CLLocationDegrees = -26.4537736
+    let lon: CLLocationDegrees = -49.1179425
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initialize Mapbox view
+        mapView = MGLMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        
+        // Set the map's center coordinate -26.4537736,-49.1179425
+        mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: lat, longitude: lon), zoomLevel: 15, animated: false)
+        view.addSubview(mapView)
     }
 
     override func didReceiveMemoryWarning() {
